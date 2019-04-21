c9b5f90faf9e3c7a44078ab3db03db08650634ce
katherine <kmarmitage@gmail.com>
2019-04-20 22:29:19 -0700

    change randomNum function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     //Returns a random number between min and max
 
     function getRandomNum(min, max) {
-        return Math.floor(Math.random() * max + min);
+        return Math.floor(Math.random() * (max - min + 1)) + min;;
     }
 
     //Resets the game
